32eb02edab6a305550ea63fb65a7b48d617cc6ae
Ardent24 <wrb.re-cords@yandex.ru>
2020-12-16 21:26:15 +0700

    finish project commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import html from './index.html';
+import './styles/style';
+// import main from "@/js/main";
+// import mocks from "@/js/mocks";
+// import createTask from "@/js/create-tasks";
+// mocks();
+// createTask();
+// main();
